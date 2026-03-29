fce575e72c366905cf927c432ee55e391c0992ec
Christopher Capobianco <ccapo@users.noreply.github.com>
2026-03-29 01:27:26 -0400

    Update C++ version, create separate function to compute sum

--- cpp/compute.cpp
+++ cpp/compute.cpp
@@ -2,9 +2,31 @@
 #include <iomanip>
 #include <sstream>
 #include <string>
+#include <vector>
+
+void compute(double threshold, double limit, std::vector<double> values) {
+  double vsum = 0.0;
+
+  // Set the output format to fixed with a single decimal place
+  std::cout << std::fixed << std::setprecision(1);
+  
+  // Iterate over input values
+  for(auto value: values) {
+      // Transform input value and update sum
+      double delta = std::min(limit - vsum, std::max(0.0, value - threshold));
+      vsum += delta;
+      
+      // Print transformed input value
+      std::cout << delta << std::endl;
+  }
+
+  // Print final sum
+  std::cout << vsum << std::endl;
+}
 
 int main(int argc, char *argv[]) {
-  double threshold = 0.0, limit = 0.0, vsum = 0.0, value;
+  double threshold = 0.0, limit = 0.0, value;
+  std::vector<double> values;
   std::string line;
 
   // Check if correct number of command line arguments are present
@@ -20,22 +42,17 @@ int main(int argc, char *argv[]) {
   // Parse limit argument
   std::string limit_str = argv[2];
   limit = std::stod(limit_str);
-  
-  // Set the output format to fixed with a single decimal place
-  std::cout << std::fixed << std::setprecision(1);
 
   // Parse the values from STDIN
   while(std::getline(std::cin, line)) {
       std::stringstream ss(line);
       if(ss >> value) {
-        // Transform input value and update sum
-        double delta = std::min(limit - vsum, std::max(0.0, value - threshold));
-        vsum += delta;
-        std::cout << delta << std::endl;
+        values.push_back(value);
       }
   }
-
-  std::cout << vsum << std::endl;
+  
+  // Compute sum
+  compute(threshold, limit, values);
 
   return 0;
 }
